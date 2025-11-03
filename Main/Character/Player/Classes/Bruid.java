110105f94cca8269d33328ed3a236eb4c3991598
wenwen-nini <aaguho20@gmail.com>
2025-11-03 23:24:57 +0800

    Refactor attackMoves initialization in Bruid class

--- Main/Character/Player/Classes/Bruid.java
+++ Main/Character/Player/Classes/Bruid.java
@@ -9,10 +9,10 @@ public class Bruid extends Player {
     setAttackPower(10);
     setSpeed(7);
    
-  	attackMoves[] = setMoves(new String[] {"1. Banana Strike (Basic + no stamina required)", 
-   												"2. Front Shield (Banana tree fronds wrap around the character, reducing incoming damage by 25% for 2 turns.)", 
-   												"3. Healing Grove (Restores moderate HP, using the banana tree's life force.)", 
-                          "4. Puso ng Saging (The druid summons a giant banana heart that explodes, dealing heavy damage and a 20% chance to stun.)"});
+  	setMoves(new String[] {"1. Banana Strike (Basic + no stamina required)", 
+   							"2. Front Shield (Banana tree fronds wrap around the character, reducing incoming damage by 25% for 2 turns.)", 
+   							"3. Healing Grove (Restores moderate HP, using the banana tree's life force.)", 
+   							"4. Puso ng Saging (The druid summons a giant banana heart that explodes, dealing heavy damage and a 20% chance to stun.)"});
 	}
  
 	public void useMoves(int moveNumber, Character target) {
