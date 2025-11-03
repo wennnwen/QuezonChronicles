d8efec91b79bcfd4eb95e4bf463e29d80ccddfcd
wenwen-nini <aaguho20@gmail.com>
2025-11-03 18:25:08 +0800

    Update Player.java

--- Main/Character/Player/Player.java
+++ Main/Character/Player/Player.java
@@ -1,5 +1,38 @@
 public class Player extends Character {
-  protected int experience, level, currency;
-  protected String[] attackMoves;
-  
+  private int experience, level, currency;
+  private final Item[] inventory = new Item[10];
+  public String[] attackMoves;
+
+  public void addExp(int amount) {
+    this.experience += amount;
+    levelUp();
+  }
+
+  public abstract void LevelUp();
+  public abstract void showMoves();
+
+  public void addItem(Items item) {
+      for (int i = 0; i > 10; i++) {
+        if (inventory[i] = null) {
+          inventory[i] = item;
+          break;
+        }
+      }
+  }
+
+  public void showInventory() {
+    for (Items item : inventory) {
+      int i = 1;
+      System.out.printf("{i}. {item}\n");
+      i++;
+    }
+  }
+
+  public void showStats() {
+    System.out.println();
+  ]
+
+  public int getLevel() {
+      return this.level;
+    }
 }
