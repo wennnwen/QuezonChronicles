6118c3c506a562b5f1d01326ed7fcaa21e7e3999
wenwen-nini <aaguho20@gmail.com>
2025-11-03 21:29:39 +0800

    Update Bruid

--- Main/Character/Player/Classes/Bruid.java
+++ Main/Character/Player/Classes/Bruid.java
@@ -9,7 +9,15 @@ public class Bruid extends Player {
     setAttackPower(10);
     setSpeed(7);
    
-  	setMoves(new String[] {"1. Banana Strike "})
-
+  	setMoves(new String[] {"1. Banana Strike (Basic + no stamina required)", 
+   												"2. Front Shield (Banana tree fronds wrap around the character, reducing incoming damage by 25% for 2 turns.)", 
+   												"3. Healing Grove (Restores moderate HP, using the banana tree's life force.)"} 
+  }
+ 
+	public void useMoves(int moveNumber, Character target) {
+    switch (moveNumber) {
+      case 1:
+        
+    }
   }
 }
