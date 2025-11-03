59a849a4838f3338e4e5ce331fef2e2a556c4f53
wenwen-nini <aaguho20@gmail.com>
2025-11-03 18:01:40 +0800

    Update Character.java

--- Main/Character/Character.java
+++ Main/Character/Character.java
@@ -1,5 +1,81 @@
-public Class Character {
+public abstract Class Character {
   private String name;
   private int maxHp, hp, stamina, mp, attackPower, defense, speed;
+
+  public abstract void useMove(int moveNumber, Character target);
   
+  public void takeDamage(int amount) {
+
+    }
+
+  public void heal(int amount) {
+
+    } 
+
+  public bool isAlive() {
+    if (hp <= 0) {
+      return false;
+    }
+  }
+
+  //getters for Stats
+  public String getName() {
+    return name;
+  }
+
+  public int getMaxHp() {
+    return maxHp;
+  }
+
+  public int getHp() {
+    return hp;
+  }
+
+  public int getStamina() {
+    return stamina;
+  }
+
+  public int getMp() {
+    return mp;
+  }
+
+  public int getAttackPower() {
+    return attackPower;
+  }
+
+  public int getDefense() {
+    return defense;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+
+  public void setMaxHp(int newMaxHp) {
+    this.maxHp = newMaxHp;
+  }
+
+  public void setHp(int newHp) {
+    this.hp = newHp;
+  }
+
+  public void setStamina(int newStamina) {
+    this.stamina = newStamina;
+  }
+
+  public void setMp(int newMp) {
+    this.mp = newMp;
+  }
+
+  public void setAttackPower(int newAttackPower) {
+    this.attackPower = newAttackPower;
+  }
+
+  public void setDefense(int newDefense) {
+    this.defense = newDefense;
+  }
+
+  public void setSpeed(int newSpeed) {
+    this.speed = newSpeed;
+  }
 }
