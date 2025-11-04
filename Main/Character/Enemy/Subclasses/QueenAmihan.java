affe4906325eb02a860a1607c04a7b6a7deed931
niljanno <githubnelljohn@gmail.com>
2025-11-04 20:55:08 +0800

    amihan and wave

--- Main/Character/Enemy/Subclasses/QueenAmihan.java
+++ Main/Character/Enemy/Subclasses/QueenAmihan.java
@@ -1,3 +1,36 @@
 package Main.Character.Enemy.Subclasses;
 
 import Main.Items.*;
+
+public class QueenAmihan extends Enemy {
+
+    public QueenAmihan() {
+        setName("Queen Amihan");
+        setMaxHp(60);
+        setHp(60);
+        setAttackPower(12); 
+        setDefense(8);
+        setSpeed(10);
+
+        expReward = 60;
+
+        // Possible Loot
+        possibleLoot = new Item[2];
+        possibleLoot[0] = new SinigangHipon();
+        possibleLoot[1] = new TropicalBreezeJuice();
+    }
+
+   @Override
+    public void enemyMove(Player player) {
+    System.out.println(getName() + " conjures a towering wave, crashing it down on " + player.getName() + "!");
+
+    int attackPower = getAttackPower();
+    
+    // Deal damage after applying effects
+    player.takeDamage(attackPower);
+    heal(getHp() + 1);
+
+}
+
+
+}
--- Main/Character/Enemy/Subclasses/WaveFiend.java
+++ Main/Character/Enemy/Subclasses/WaveFiend.java
@@ -1,3 +1,40 @@
 package Main.Character.Enemy.Subclasses;
 
 import Main.Items.*;
+
+public class WaveFiend extends Enemy {
+
+    public WaveFiend() {
+        setName("Wave Fiend");
+        setMaxHp(60);
+        setHp(60);
+        setAttackPower(12); 
+        setDefense(8);
+        setSpeed(10);
+
+        expReward = 60;
+
+        // Possible Loot
+        possibleLoot = new Item[2];
+        possibleLoot[0] = new SinigangHipon();
+        possibleLoot[1] = new TropicalBreezeJuice();
+    }
+
+   @Override
+    public void enemyMove(Player player) {
+    System.out.println(getName() + " conjures a towering wave, crashing it down on " + player.getName() + "!");
+
+    int attackPower = getAttackPower();
+
+    // Slamming Waves skill: Chance to lower player's attack power (uses debuff system)
+    double weakenChance = 0.33; // 20% chance
+    if (Math.random() < weakenChance) {
+        player.applyDebuff("attack down", 2); // Applies "attack down" debuff for 2 turns
+    }
+
+    // Deal damage after applying effects
+    player.takeDamage(attackPower);
+}
+
+
+}
