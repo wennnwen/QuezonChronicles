3d86b1ad2b0ca05f86f5411731a5e863c483ab56
skyrvou <168846889+skyrvou@users.noreply.github.com>
2025-11-04 20:11:26 +0800

    items

--- Main/Character/Enemy/Subclasses/SweetVendor.java
+++ Main/Character/Enemy/Subclasses/SweetVendor.java
@@ -27,6 +27,14 @@ public class SweetVendor extends Enemy {
 
         // Basic single move
         int damage = getAttackPower();
+        
+        double chance = Math.random();  // gives a random number between 0.0 and 1.0
+        if (chance < 0.20) { // 20% chance
+            damage *= 2;
+            System.out.println("💥 Critical Hit! The candy explodes with extra force!");
+        }
+
+
         player.takeDamage(damage);
         
     }
