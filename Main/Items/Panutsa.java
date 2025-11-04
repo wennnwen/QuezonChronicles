50a6a973511aeb272e255d24fa066160188ab259
Jamilano Red <moistyclothing@gmail.com>
2025-11-04 09:51:51 +0800

    Panutsa item classes

--- Main/Items/Panutsa.java
+++ Main/Items/Panutsa.java
@@ -1,1 +1,16 @@
+public class Panutsa extends Items{
 
+    public Panutsa() {
+        setName("Panutsa");
+        setDescription("A sweet treat that boosts your defense by +5 per turn for 2 turns.");
+    }
+
+    @Override
+    public void useItem(Player player) {
+        int defenseBoost = 5;
+        int duration = 2;
+
+        System.out.println(player.getName() + " eats the Panutsa!")
+        player.addDefenseBuff(defenseBoost, duration);
+    }
+}
