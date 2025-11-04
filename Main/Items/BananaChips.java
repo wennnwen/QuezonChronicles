f700aecd29d45d8d47441d137ec5a79c10db0b90
niljanno <githubnelljohn@gmail.com>
2025-11-04 17:46:42 +0800

    Update BananaChips

--- Main/Items/BananaChips.java
+++ Main/Items/BananaChips.java
@@ -1,1 +1,16 @@
+public class BananaChips extends Items {
 
+    public BananaChips() {
+        setName("Banana Chips");
+        setDescription("(Crispy banana chips. Quick energy boost grants +2 Speed for one turn.)");
+    }
+
+    @Override
+    public void useItem(Player player) {
+        int speedBoost = 2;
+
+        System.out.println(player.getName() + " ate the Banana Chips!");
+        System.out.println(player.getName() + " increased their speed by 2!");
+        player.addSpeed(speedBoost);
+    }
+}
