008188fc13bba2598483a5a210203663fc7d8be7
wenwen-nini <aaguho20@gmail.com>
2025-11-04 13:17:55 +0800

    Update Mage

--- Main/Character/Player/Classes/Mage.java
+++ Main/Character/Player/Classes/Mage.java
@@ -10,7 +10,70 @@ public class Mage extends Player {
    
   	setMoves(new String[] {"1. Fire Ball (Basic + no mana required)", 
    			"2. LambaShield (Creates a barrier that reduces incoming damage by 30% for 2 turns. 10 MP)", 
-            "3. Mana Surge (Regenerates 10 MP instantly.)",
+            "3. Mana Surge (Regenerates 20 MP instantly.)",
             "4. Pinagong Storm (Calls down hard pinagong breads on all enemies, dealy heavy damage. 18 MP)"});
    }
+
+   public void useMoves(int moveNumber, Character target) {
+      switch(moveNumber) {
+         case 1:
+            System.out.println(getName() + " cast a Fire Ball!");
+            target.takeDamage(getAttackPower());
+            break;
+
+         case 2:
+            if (getMp() >= 10) {
+               System.out.println(getName() + " cast a LambaShield!");
+               setMp(getMp() - 10);
+               addTemporaryDefenseBoost((int)(getDefense() * 0.30), 2);
+            }
+            else {
+               System.out.println("Not enough Mana!");
+            }
+            break;
+
+         case 3:
+            System.out.println(getName() + " cast a Mana Surge!");
+            setMp(getMp() + 20);
+			   System.out.println("Mana Restored by 20 points!");
+            break;
+
+         case 4:
+            if (getMp() >= 18) {
+               System.out.println(getName() + " cast a Pinagong Storm!");
+               int damage = getAttackPower() + (int)(getAttackPower() * 0.5);
+				   target.takeDamage(damage);
+            }
+            else {
+               System.out.println("Not enough Mana!");
+            }
+            break;
+      }
+   }
+
+   public void showMoves() {
+      for (String moves : attackMoves) {
+         System.out.println(moves);
+      }
+   }
+
+   public void showStats() {
+		System.out.println("====Stats====");
+		System.out.println("Health: " + getHp() + "/" + getMaxHp());
+		System.out.println("Mana: " + getMp());
+		System.out.println("Defense: " + getDefense());
+		System.out.println("Attack Power: " + getAttackPower());
+		System.out.println("Speed: " + getSpeed());
+		System.out.println("Experience: " + getExp() + "/" + getNextExpLevel());
+		System.out.println("Level: " + getLevel());
+	}
+
+   public void levelStats() {
+ 		setMaxHp(getMaxHp() + 5);
+		setHp(getHp() + 5);
+		setMp(getMp() + 5);
+		setDefense(getDefense() + 1);
+		setAttackPower(getAttackPower() + 2);
+		setSpeed(getSpeed() + 1);
+	}
 }
