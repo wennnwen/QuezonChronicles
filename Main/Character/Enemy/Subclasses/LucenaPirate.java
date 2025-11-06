06f5e1e91f929b881d0780b980ed4e5ea1317b1c
Jamilano Red <moistyclothing@gmail.com>
2025-11-06 08:39:07 +0800

    LucenaPirate, LopezWolf

---
+++ Main/Character/Enemy/Subclasses/LucenaPirate.java
@@ -0,0 +1,41 @@
+package Main.Character.Enemy.Subclasses;
+
+import Main.Items.*;
+
+public class LucenaPirate extends Enemy{
+
+    public LucenaPirate(){
+        setName("Lucena Pirate");
+        setMaxHp(30);
+        setHp(30);
+        setAttackPower(6);
+        setDefense(6);
+        setSpeed(10);
+
+        // Experience reward
+        expReward = 40;
+
+        //Possible Loot
+        possibleLoot = new Item[2];
+        possibleLoot[0] = new Chami();
+        possibleLoot[1] = new CocoJam();
+    }
+
+    @Override
+    public void enemyMove(Player player) {
+    System.out.println(getName() + " lunges towards " + player.getName() + " fiercely!");
+
+    int baseDamage = getAttackPower();
+    if (baseDamage < 0) baseDamage = 0;
+
+    // Critical hit mechanic
+    double critChance = 0.2;
+    double critRoll = Math.random();
+    if (critRoll < critChance) {
+        baseDamage *= 2;
+        System.out.println("Critical hit!");
+    }
+
+        player.takeDamage(baseDamage);
+    }
+}
