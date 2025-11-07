518d1a7dafe61b692a7dfe69168e8f0409de4c4c
niljanno <githubnelljohn@gmail.com>
2025-11-07 13:35:40 +0000

    Improved UI During Battle

--- Main/game/BattleSystem.java
+++ Main/game/BattleSystem.java
@@ -47,7 +47,7 @@ public class BattleSystem {
                     if (player.isAlive() && enemy.isAlive()) {
                         System.out.println("========================================================================\nPlayer Stats:\t\t\tEnemy Stats:");
                         System.out.println("Hp: " + player.getHp() + "/" + player.getMaxHp() + "\t\t\tHp: " + enemy.getHp() + "/" + enemy.getMaxHp());
-                        System.out.println("Stamina: " + player.getStamina() + "/" + player.getMaxStamina() + " | Mp: " + player.getMp() + "/" + player.getMaxMp() + "\n");
+                        System.out.println("Stamina: " + player.getStamina() + "/" + player.getMaxStamina() + " | Mp: " + player.getMp() + "/" + player.getMaxMp());
                         playerTurn(player, enemy);
                     }
                 }
